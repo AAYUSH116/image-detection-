9ba190dbae854745c91ea58efec4bbf933faefa7
AAYUSH116 <82816625+AAYUSH116@users.noreply.github.com>
2021-07-03 13:10:17 +0530

    Add files via upload

---
+++ basket.js
@@ -0,0 +1,27 @@
+status="";
+object="";
+function preload(){
+image1=loadImage('basket.jpg');
+}
+function setup(){
+    canvas=createCanvas(480,480);
+    canvas.center()
+    objectDetection=ml5.objectDetection('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML='Status: object detecting';
+}
+function draw(){
+    image(image1,0,0,480,480)
+}
+function modelLoaded(){
+    console.log('cocossd is initialized')
+    status=true;
+    objectDetection.detect(image1,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log('error')
+    }
+    console.log(results);
+    object=results;
+}
